899d2a98ca3c53492c337987f36ebad76789f68c
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-05-14 10:13:46 -0300

    feat: delete print of user/password

--- Django/sospet/core/views.py
+++ Django/sospet/core/views.py
@@ -19,7 +19,6 @@ def submit_login(request):
   if request.POST:
     username = request.POST.get('username')
     password = request.POST.get('password')
-    print(username, password)
     user = authenticate(username = username, password = password)
     if user is not None:
       login(request, user)
