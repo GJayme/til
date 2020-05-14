8100fb9de8156a16ea52fd793267a1778eef3837
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-05-14 12:20:49 -0300

    feat: image file

--- Django/sospet/core/views.py
+++ Django/sospet/core/views.py
@@ -2,32 +2,38 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from .models import Pet
 # Create your views here.
 
-#Dashbord
+
 @login_required(login_url='/login')
-def index(request):
-  return render(request, 'index.html')
+#List All Pets
+def list_all_pets(request):
+    pet = Pet.objects.filter(active=True)
+    return render(request, 'list.html', {'pet': pet})
+
 
 #Login Page
 def login_user(request):
-  return render(request, 'login.html')
+    return render(request, 'login.html')
 
-#Login Submit 
+
+#Login Submit
 def submit_login(request):
-  if request.POST:
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(username = username, password = password)
-    if user is not None:
-      login(request, user)
-      return redirect('/')
-    else:
-      messages.error(request, 'Usuário ou senha inválida. Favor tentar novamente.')
-  return redirect('/login')
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(
+                request, 'Usuário ou senha inválida. Favor tentar novamente.')
+    return redirect('/login')
+
 
 # Logout
 def logout_user(request):
-  logout(request)
-  return redirect('/login')
+    logout(request)
+    return redirect('/login')
