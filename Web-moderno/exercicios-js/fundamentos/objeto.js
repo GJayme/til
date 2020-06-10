997ffb14d8260cea445a08de1da5eda43346234b
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-06-10 16:51:23 -0300

    feat: object in JS

---
+++ Web-moderno/exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {};
+prod1.nome = "Celular Ultra Mega";
+prod1.preco = 4998.9;
+prod1["Desconto Legal"] = 0.4; //Evitar atributos com espaço
+
+console.log(prod1);
+
+const prod2 = {
+  nome: "Camisa Polo",
+  preco: 79.9,
+};
+
+('{"nome": Jhon, "preco": 79.99 }'); // JSON
+
+console.log(prod2);
