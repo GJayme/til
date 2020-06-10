226e14c9cd7dcb95809ba719d8650982a79a029f
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-06-10 16:52:43 -0300

    feat: typing in JS

---
+++ Web-moderno/exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,9 @@
+// Tipagem Fraca: as variaveis podem variar e ter multiplos tipos
+
+let qualquer = "Legal";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
