ef517916a42b30627b5460963f5e3ffc3591e709
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-07-13 12:47:18 -0300

    feat: oo principles in JS

---
+++ Web-moderno/exercicios-js/objeto/oo.js
@@ -0,0 +1,23 @@
+// CODIGO NÃO EXECUTÁVEL!!
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    // ...
+  }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// PRINCIPIOS IMPORTANTE:
+// 1. ABSTRAÇÃO: pegar um objeto do mundo real e traduzir para um objeto detro do software.
+// 2. ENCAPSULAMENTO: ter os detalhes de implementação escondidos e mostrar para quem precisa 
+//    usar o sistema, mostrar uma interface simples para poder interagir com o objeto.
+// 3. HERANÇA(PROTOTYPE): você recebe atributos e comportamento de um objeto pai.(É UM)
+// 4. POLIMORFISMO: multiplas formas
