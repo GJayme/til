bab71c5c614dbaedb8577e8b684c8b7d33b9b69a
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-07-27 13:45:02 -0300

    feat: forEach

---
+++ Web-moderno/exercicios-js/array/foreach1.js
@@ -0,0 +1,10 @@
+const aprovados = ['Gabriel', 'Charles', 'Vinicius', 'Leonardo']
+
+aprovados.forEach(function (nome, indice) {
+  console.log(`${indice + 1}) ${nome}`)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
