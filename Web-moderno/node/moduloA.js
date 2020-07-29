33fba8d5eb1bc6551788aebbc147fdabd896ce0f
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-07-29 16:08:26 -0300

    feat: modules in node

---
+++ Web-moderno/node/moduloA.js
@@ -0,0 +1,4 @@
+// Formas de exportar funções de um modulo do node
+this.ola = 'Fala Pessoa';
+exports.bemVindo = 'Bem vindo ao node!';
+module.exports.ateLogo = 'Até o próximo exemplo';
---
+++ Web-moderno/node/moduloB.js
@@ -0,0 +1,8 @@
+// Outra forma de exportar
+module.exports = {
+  bomDia: 'Bom dia',
+  boaNoite() {
+    return 'Boa noite';
+  },
+
+}
