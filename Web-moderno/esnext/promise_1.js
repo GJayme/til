bb93eda428c78ac8a9e76c9d5767aa93bc78ac16
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-08-07 14:53:54 -0300

    feat: review promises

---
+++ Web-moderno/esnext/promise_1.js
@@ -0,0 +1,12 @@
+const primeiroElemento = array => array[0]
+const primeiraLetra = string => string[0]
+const letraMinuscula = letra => letra.toLowerCase()
+
+new Promise(function (resolve) {
+  resolve(['Ana', 'Daniel', 'Leonardo'])
+})
+
+  .then(primeiroElemento)
+  .then(primeiraLetra)
+  .then(letraMinuscula)
+  .then(console.log)
