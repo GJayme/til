7cb52cc5791e21038c499af68b489717e9536060
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-09-11 15:25:06 -0300

    feat: adding post request

--- Django-Rest-Framework/cursos/views.py
+++ Django-Rest-Framework/cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -13,6 +14,12 @@ class CursoAPIView(APIView):
     serializers = CursoSerializer(cursos, many=True)
     return Response(serializers.data)
 
+  def post(self, request):
+    serializers = CursoSerializer(data=request.data)
+    serializers.is_valid(raise_exception=True)
+    serializers.save()
+    return Response(serializers.data, status=status.HTTP_201_CREATED)
+
 
 class AvaliacaoAPIView(APIView):
   """
@@ -22,3 +29,9 @@ class AvaliacaoAPIView(APIView):
     avaliacoes = Avaliacao.objects.all()
     serializers = AvaliacaoSerializer(avaliacoes, many=True)
     return Response(serializers.data)
+  
+  def post(self, request):
+    serializers = AvaliacaoSerializer(data=request.data)
+    serializers.is_valid(raise_exception=True)
+    serializers.save()
+    return Response(serializers.data, status=status.HTTP_201_CREATED)
