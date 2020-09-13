d1675723f6bd728b10c7337f8cd3fee761a6d80f
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-09-13 14:26:13 -0300

    feat: creating API with generics

--- Django-Rest-Framework/cursos/views.py
+++ Django-Rest-Framework/cursos/views.py
@@ -1,37 +1,24 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 from .models import Curso, Avaliacao
-from .serializers import CursoSerializer, AvaliacaoSerializer
-
-class CursoAPIView(APIView):
-  """
-  API de Crusos da Geek
-  """
-  def get(self, request):
-    cursos = Curso.objects.all()
-    serializers = CursoSerializer(cursos, many=True)
-    return Response(serializers.data)
-
-  def post(self, request):
-    serializers = CursoSerializer(data=request.data)
-    serializers.is_valid(raise_exception=True)
-    serializers.save()
-    return Response(serializers.data, status=status.HTTP_201_CREATED)
+from. serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class AvaliacaoAPIView(APIView):
-  """
-  API de Avaliações da Geek
-  """
-  def get(self, request):
-    avaliacoes = Avaliacao.objects.all()
-    serializers = AvaliacaoSerializer(avaliacoes, many=True)
-    return Response(serializers.data)
-  
-  def post(self, request):
-    serializers = AvaliacaoSerializer(data=request.data)
-    serializers.is_valid(raise_exception=True)
-    serializers.save()
-    return Response(serializers.data, status=status.HTTP_201_CREATED)
+class CursosAPIView(generics.ListCreateAPIView):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+
+
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
+
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
