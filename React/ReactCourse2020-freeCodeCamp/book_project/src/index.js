3164f9929d9ccf10bc685de6b49f4c714da5c50a
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-10-09 16:16:05 -0300

    feat: props and children

--- React/ReactCourse2020-freeCodeCamp/book_project/src/index.js
+++ React/ReactCourse2020-freeCodeCamp/book_project/src/index.js
@@ -3,29 +3,52 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+//Setup vars
+const firstBook = {
+  author: 'Neil Gaiman',
+  title: 'Absolute Sandman - Vol.1',
+  img:
+    'https://m.media-amazon.com/images/I/61xQCxJ1SqL._AC_UY218_.jpghttps://m.media-amazon.com/images/I/61xQCxJ1SqL._AC_UY218_.jpg',
+};
+
+const secondBook = {
+  author: 'Doug Moench e Kelley Jones',
+  title: 'Batman, Vol.1',
+  img: 'https://m.media-amazon.com/images/I/819ZMpwTNOL._AC_UY218_.jpg',
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings
+          elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis,
+          espiritis santis.
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const author = 'Neil Gaiman';
-
-const Book = () => {
-  const title = 'Absolute Sandman - Vol.1';
+const Book = (props) => {
+  const { img, title, author, children } = props;
 
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/61xQCxJ1SqL._AC_UY218_.jpghttps://m.media-amazon.com/images/I/61xQCxJ1SqL._AC_UY218_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-      {/* <p>{let x = 6}</p> */}
-      <p>{6 + 6}</p>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
