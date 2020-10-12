9ff0d002460615912079e3c5e560a7565897ec8e
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-10-12 17:29:01 -0300

    feat: principle of synthetic events on react

--- React/ReactCourse2020-freeCodeCamp/book_project/src/index.js
+++ React/ReactCourse2020-freeCodeCamp/book_project/src/index.js
@@ -31,7 +31,7 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book key={book.id} {...book}></Book>;
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
@@ -40,11 +40,34 @@ function BookList() {
 const Book = ({ img, title, author }) => {
   // const { img, title, author } = props;
 
+  //Attributes, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+    alert('hello world');
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}> {title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+        reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        more complex example
+      </button>
     </article>
   );
 };
